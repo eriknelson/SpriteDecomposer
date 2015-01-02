2b3856c9dc72e51c331ce72da5cae7dca271abc4
Erik Nelson <erik@nelsk.com>
2015-01-01 20:01:37 -0500

    Reconfigure plugin path for release builds
    My original port continued to fail to start due to hard paths
    of dependent QT dylibs being baked into the executable. The primary
    executable and all dependent QT libs needed to be modified to load
    their respective dependencies via relative paths to facilitate
    a self contained app package.
    
    This process is mostly a post-build step. However, a lone QT plugin,
    libqcocoa, strangely does not show up as a dependency with an
    `otool -L $lib`. This app modification reconfigures QtCore to
    also use a path relative to the executable.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include "application.h"
 #include "interface.h"
+#include <assert.h>
 
 int main(int argc, char *argv[])
 {
+
+////////////////////////////////////////////////////////////
+// NOTE: Conditionally setup plugin path to be located at
+// $appPackageRoot/Plugins. Something of a hack for redistributing QT
+// dylib dependencies with the app package itself.
+////////////////////////////////////////////////////////////
+#ifndef QT_DEBUG
+    QDir dir(argv[0]); //appdir/Contents/MacOS/appname
+    assert(dir.cdUp());
+    assert(dir.cdUp());
+    assert(dir.cd("Plugins"));
+    QCoreApplication::setLibraryPaths(QStringList(dir.absolutePath()));
+#endif
+////////////////////////////////////////////////////////////
+
     Application a(argc, argv);
     Q_INIT_RESOURCE(lang);
     a.loadTranslations(":/lang");
